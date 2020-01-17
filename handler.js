0cb657415a74bef33f2a8b260396ba80be105d16
Sean McPheeters <32907312+Bostonfan06@users.noreply.github.com>
2020-01-17 10:23:58 -0800

    Updated Server Settings

--- handler.js
+++ handler.js
@@ -1,24 +1,44 @@
 var serverless = require("serverless-http");
 var hbs = require("hbs");
+var exphbs  = require('express-handlebars');
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
 var app = express();
 
-var port = 3000;
+var port = 8080;
+
+var hbs = exphbs.create({
+  defaultLayout: 'main', 
+  extname: '.hbs', 
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials')
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
 app.use(bodyParser.json());
-app.set("view engine", "hbs");
 
+// Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+app.engine('.hbs', hbs.engine); 
+
+// Serving Static Files
 app.use(express.static(__dirname + '/public'));
 
+// Routes
 app.get("/", function(req, res) {
-  res.status(200).render("index");
+  res.status(200).render("index", {
+    title: 'Home Page'
+  });
 });
 
 app.get("/about", function(req, res) {
-  res.status(200).render("about");
+  res.status(200).render("about", {
+    title: 'About'
+  });
 });
 
 app.listen(port, () => console.log('Listening on port ${port}!'));
